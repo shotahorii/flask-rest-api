e89cdfc0f23a1df3ac090d7e8978aec270622c07
shotahorii <sh.sinker@gmail.com>
2020-06-18 00:49:34 +0100

    data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_restful.utils import cors
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -25,8 +26,17 @@ class TodoSimple(Resource):
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}
 
+class DublinRestaurants(Resource):
+
+    @cors.crossdomain(origin='*')
+    def get(self):
+        with open('storage/dublin_restaurants.json') as f:
+            data = json.load(f)
+            return data
+
 api.add_resource(HelloWorld, '/')
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(TodoSimple, '/api/<string:todo_id>')
+api.add_resource(DublinRestaurants, '/api/dublinrestaurants')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
