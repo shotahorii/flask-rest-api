e889fbabca6eff8db06f438ac060666a96d80a39
shotahorii <sh.sinker@gmail.com>
2020-06-18 01:12:19 +0100

    index

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from flask_restful import Resource, Api
 from flask_restful.utils import cors
 import json
@@ -12,7 +12,8 @@ class HelloWorld(Resource):
 
     @cors.crossdomain(origin='*')
     def get(self):
-        return render_template('index.html')
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('index.html'),200,headers)
 
 class TodoSimple(Resource):
 
